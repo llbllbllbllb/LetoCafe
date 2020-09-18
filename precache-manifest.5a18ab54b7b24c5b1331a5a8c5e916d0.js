fae8f5961ddaeaf1680c9531403d7fb11c273f23
llbllbllbllb <295319725@qq.com>
2020-09-17 21:06:39 -0400

    Updates

---
+++ precache-manifest.5a18ab54b7b24c5b1331a5a8c5e916d0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff730b942767a9f7bb60e2f3a2b8ebc",
+    "url": "/LetoCafe/index.html"
+  },
+  {
+    "revision": "3551f9105410c79aecfe",
+    "url": "/LetoCafe/static/css/main.d1400043.chunk.css"
+  },
+  {
+    "revision": "8cef6378ef5f56072083",
+    "url": "/LetoCafe/static/js/2.481511ec.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/LetoCafe/static/js/2.481511ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3551f9105410c79aecfe",
+    "url": "/LetoCafe/static/js/main.9b4d7aa6.chunk.js"
+  },
+  {
+    "revision": "64aba465cf46d5b75f87",
+    "url": "/LetoCafe/static/js/runtime-main.ebc256c2.js"
+  },
+  {
+    "revision": "87b539e59c4b1dcc8c7db3f0318ce474",
+    "url": "/LetoCafe/static/media/Catering_1.87b539e5.jpg"
+  },
+  {
+    "revision": "b42441c32a79f0debad5d40c0fad8cf9",
+    "url": "/LetoCafe/static/media/catering_banner.b42441c3.jpg"
+  },
+  {
+    "revision": "04d623a303b69d9d434bf801354615b0",
+    "url": "/LetoCafe/static/media/contact_banner.04d623a3.jpg"
+  },
+  {
+    "revision": "3e0bba484c8d3a149dbdebeda3a605bb",
+    "url": "/LetoCafe/static/media/leto-cakes01163-300x300.3e0bba48.jpg"
+  },
+  {
+    "revision": "c11cdeb22af55dcc021e14bd46e5fc1b",
+    "url": "/LetoCafe/static/media/leto-cakes02152-300x300.c11cdeb2.jpg"
+  },
+  {
+    "revision": "b8487e95b0aa5f03b39515215c3292a7",
+    "url": "/LetoCafe/static/media/main_banner_1.b8487e95.jpg"
+  },
+  {
+    "revision": "a96bde4214923dd687efb0cc8203eadf",
+    "url": "/LetoCafe/static/media/main_banner_2.a96bde42.jpg"
+  },
+  {
+    "revision": "d57aebe99ea0ae5b09e9eb4f6a1e3ef0",
+    "url": "/LetoCafe/static/media/main_banner_3.d57aebe9.jpg"
+  },
+  {
+    "revision": "8216c13e144bb595fa43217b70ea1a92",
+    "url": "/LetoCafe/static/media/navbarbg.8216c13e.jpg"
+  },
+  {
+    "revision": "4a4dfe99c33405295a464a151d3a8095",
+    "url": "/LetoCafe/static/media/photo5-300x300.4a4dfe99.jpg"
+  }
+]);
